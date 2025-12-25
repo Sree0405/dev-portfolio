a6533f35e36d11a9899555cee3495d9fd360b991
Sreekanth <sreekanth04052005@gmail.com>
2025-12-25 12:16:36 +0530

    Route Settup

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,12 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ImmersivePage from "./pages/ImmersivePage";
 import NotFound from "./pages/NotFound";
-
+import AboutPage from "./pages/AboutPage";
+import Home from "./pages/Home";
+import ProjectsPage from "./pages/ProjectsPage";
+import SkillsPage from "./pages/SkillsPage";
+import ContactPage from "./pages/ContactPage";
+import ExperiencePage from "./pages/ExperiencePage";
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -15,9 +20,16 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ImmersivePage />} />
+          {/* <Route path="/" element={<ImmersivePage />} /> */}
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/immersive" element={<ImmersivePage />} />
+          <Route path="/experience" element={<ExperiencePage />} />
+          <Route path="/projects" element={<ProjectsPage />} />
+          <Route path="/skills" element={<SkillsPage/>}/>
           <Route path="*" element={<NotFound />} />
+          <Route path="/contact" element={<ContactPage/>}/>
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
--- src/pages/ImmersivePage.tsx
+++ src/pages/ImmersivePage.tsx
@@ -5,7 +5,8 @@ const ImmersivePage = () => {
   return (
     <div className="relative w-full min-h-screen overflow-y-auto overflow-x-hidden bg-background">
       {/* Navigation hidden for immersive experience */}
-      {/* <Navigation /> */}
+      <Navigation />
+      hi
       <ImmersiveScene />
     </div>
   );
